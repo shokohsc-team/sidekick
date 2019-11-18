95bcbe86ddf688de0db569d773681bbee9c38c9a
shokohsc <shokohsc@gmail.com>
2019-11-18 15:48:16 +0100

    wip

--- src/services/sendMatterMostMessage.js
+++ src/services/sendMatterMostMessage.js
@@ -9,12 +9,19 @@ async function sendMatterMostMessage(msg) {
     if (void 0 !== config.mattermostWebhookUrl && isValid) {
         // Server thread/INFO: Group `time` Group `type` Group `message`
         const regex = /^(?<time>\[\d{2}:\d{2}:\d{2}\]) (?<type>\[(Server thread|Async Chat Thread - #\d+)\/INFO\]): (?<message>.+)$/g;
-        const { groups: { message } } = regex.exec(msg);
-        await mattermost.send({
-            text: message,
-            channel: '#minecraft',
-            username: 'minecraft'
-        });
+        try {
+            const { groups: { message } } = regex.exec(msg);
+            await mattermost.send({
+                text: message,
+                channel: '#minecraft',
+                username: 'minecraft'
+            });
+        } catch (err) {
+            console.log('Cannot post to mattermost, msg: ');
+            console.log(msg)
+            console.log('Error: ');
+            console.log(err)
+        }
     }
 };
 

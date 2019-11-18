698d4079a7ec06ba17d5fb8e38eccb0d0c3d9c53
shokohsc <shokohsc@gmail.com>
2019-11-18 15:22:06 +0100

    wip

--- src/services/sendMatterMostMessage.js
+++ src/services/sendMatterMostMessage.js
@@ -8,7 +8,7 @@ async function sendMatterMostMessage(msg) {
     const isValid = await minecraftLogValidate(msg);
     if (void 0 !== config.mattermostWebhookUrl && isValid) {
         // Server thread/INFO: Group `time` Group `type` Group `message`
-        const regex = /^(?<time>\[\d{2}:\d{2}:\d{2}\]) (?<type>\[(Server thread|Async Chat Thread - \#\d+)\/INFO\]): (?<message>.+)$/g;
+        const regex = /^(?<time>\[\d{2}:\d{2}:\d{2}\]) (?<type>\[(Server thread|Async Chat Thread - #\d+)\/INFO\]): (?<message>.+)$/g;
         const { groups: { message } } = regex.exec(msg);
         await mattermost.send({
             text: message,

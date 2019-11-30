41da0cb0aed9fa833c5c4349bc1b1aebb038201d
shokohsc <shokohsc@gmail.com>
2019-11-30 21:18:52 +0100

    added worldborder regex filter

--- src/services/minecraftLogValidate.js
+++ src/services/minecraftLogValidate.js
@@ -6,7 +6,7 @@ async function validate(msg, regexes) {
 
 async function minecraftLogValidate(message) {
     const regexes = [
-        /^\[\d{2}:\d{2}:\d{2}\] \[Server thread\/INFO\]: (?!(.+: (\d+%$|\d+$|\d+\.\d+$|\d+ ms$|\d+ms$|true$|false$)|(.+loading it automatically$)|(.+World Settings For)|(\[dynmap\])|(Loading properties)|(This server is running)|(Console input is disabled)|(Using \d+ threads)|(Debug logging is disabled)|(Default game type)|(Generating keypair)|(Reloading ResourceManager)|(Loaded \d+ recipes)|(Using epoll)|(Preparing start region for dimension)|(Entity \w+ Range)|(\[Rcon)|(Saving chunks)|(ThreadedAnvilChunkStorage)|(Server permissions)|(Starting remote control))).+$/,
+        /^\[\d{2}:\d{2}:\d{2}\] \[Server thread\/INFO\]: (?!(.+: (\d+%$|\d+$|\d+\.\d+$|\d+ ms$|\d+ms$|true$|false$)|(.+loading it automatically$)|(.+World Settings For)|(\[dynmap\])|(\[WorldBorder\])|(Loading properties)|(This server is running)|(Console input is disabled)|(Using \d+ threads)|(Debug logging is disabled)|(Default game type)|(Generating keypair)|(Reloading ResourceManager)|(Loaded \d+ recipes)|(Using epoll)|(Preparing start region for dimension)|(Entity \w+ Range)|(\[Rcon)|(Saving chunks)|(ThreadedAnvilChunkStorage)|(Server permissions)|(Starting remote control))).+$/,
         /^\[\d{2}:\d{2}:\d{2}\] \[Server-Worker-\d+\/INFO\]: (?!.+: (\d+%$|\d+$|\d+\.\d+$|\d+ ms$|\d+ms$|true$|false$)).+$/,
         /^\[\d{2}:\d{2}:\d{2}\] \[Async Chat Thread - #\d+\/INFO\]: (?=\<.+\>).+$/,
     ];
